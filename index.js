6dfa32239e7fcacc1d8990cef2d6e88dc60f5b94
umb0000 <sandwich1038@gmail.com>
2020-03-24 02:04:06 +0900

    #2.6

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ const PORT = 4000;
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
-
   const handleHome = (req, res) => res.send("Hello from my home");
   
   const handleProfile = (req, res) => res.send("You are on my profile");
+  
+  const betweenHome = (req, res, next) => {
+    console.log("Between");
+    next();
+  };
+  
+  app.use(betweenHome);
   
   app.get("/", handleHome);
   
   app.get("/profile", handleProfile);
   
-
 app.listen(PORT, handleListening);
