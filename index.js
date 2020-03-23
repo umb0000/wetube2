0f86765ca9d4f074b8b76568c9c3a1b2e0c027b4
umb0000 <sandwich1038@gmail.com>
2020-03-24 00:38:50 +0900

    #2.5 before nodemon

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
+
+
+  const handleHome = (req, res) => res.send("Hello from my ass");
+  
+  const handleProfile = (req, res) => res.send("You are on my profile");
+  
+  app.get("/", handleHome);
+  
+  app.get("/profile", handleProfile);
+  
 
 app.listen(PORT, handleListening);
