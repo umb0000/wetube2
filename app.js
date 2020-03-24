59ed89bb7ce8f84ec5873b749caedb262b9e72bc
umb0000 <sandwich1038@gmail.com>
2020-03-24 15:05:55 +0900

    #2.9

--- app.js
+++ app.js
@@ -3,23 +3,19 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router"; //router defalt로 export 하지 않았음//
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 const app = express();
 
-  const handleHome = (req, res) => res.send("Hello from my home");
-  
-  const handleProfile = (req, res) => res.send("You are on my profile");
-  
   app.use(cookieParser());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(helmet());
   app.use(morgan("dev"));
 
-  app.get("/", handleHome);
-  
-  app.get("/profile", handleProfile);
-
+  app.use("/", globalRouter);
   app.use("/user", userRouter);
+  app.use("/video", videoRouter);
 
   export default app;
